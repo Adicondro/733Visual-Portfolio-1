f10480e4b9335236570aff993c368d31378c714a
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2024-07-06 22:51:56 +0700

    creating new repo

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import About from "./pages/About"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+
+export default function App() {
+  return (
+    <>
+      <Header/>
+      <Routes>
+        <Route path="/" Component={Homepage}/>
+        <Route path="/about" Component={About}/>
+      </Routes>
+      <Footer/>
+    </>
+  )
+}
