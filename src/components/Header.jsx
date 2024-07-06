6a9b2fb141205b5bd595efa65c751b4934370423
Adicondro Yusuf <adicondro.yusuf@gmail.com>
2024-07-07 00:56:09 +0700

    adding navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+        {/* Navbar Box */}
+      <header className='flex flex-row-reverse items-center h-32 p-8 space-x-4'>
+
+        {/* Organize Navbar Item */}
+
+            {/* Organize Item Alignment */}
+            <div className='flex space-x-4'>
+                
+                <Link to="/">
+                    <Button className="text-2xl" variant="ghost">
+                        Home
+                    </Button>
+                </Link>
+                <Link to="/about">
+                    <Button className="text-2xl" variant="ghost">
+                        Works
+                    </Button>
+                </Link>
+            </div>
+      </header>
     </div>
   )
 }
